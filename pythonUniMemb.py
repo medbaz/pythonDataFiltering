33ed44bea6bdf58b7ab2abba6e240f9f23228ec1
medbaz <mohamed.bazber11@gmail.com>
2024-12-28 13:58:44 +0100

    cleaning code

--- pythonUniMemb.py
+++ pythonUniMemb.py
@@ -70,37 +70,6 @@ def cleanFiles(currentMem, exMem):
         
     
     pass # Remove this line when done implementation
-"""
-def cleanFiles(currentMem,exMem):
-    with open(currentMem,'r+') as writeFile: 
-        with open(exMem,'a+') as appendFile:
-            #get the data
-            writeFile.seek(0)
-            members = writeFile.readlines()
-            #remove header
-            header = members[0]
-            members.pop(0)
-                
-            inactive = [member for member in members if ('no' in member)]
-            '''
-            The above is the same as 
-
-            for member in members:
-            if 'no' in member:
-                inactive.append(member)
-            '''
-            #go to the beginning of the write file
-            writeFile.seek(0) 
-            writeFile.write(header)
-            for member in members:
-                if (member in inactive):
-                    appendFile.write(member)
-                else:
-                    writeFile.write(member)      
-            writeFile.truncate()
-
-"""
-
 
 
 
